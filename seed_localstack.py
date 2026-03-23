e1b1816de19a523173c4b9cdc7bc53700beca730
alazarsetarge1-a11y <alazarsetarge1@gmail.com>
2026-03-23 15:46:33 -0500

    Add vulnerable EC2 security group to seed
    Extend LocalStack seeding script to create an intentionally misconfigured EC2 security group and open SSH (port 22) to 0.0.0.0/0. The change also leaves the existing IAM user creation (no MFA) and updates final print statements to report the created security group and that LocalStack is ready for exercises.

--- seed_localstack.py
+++ seed_localstack.py
@@ -24,6 +24,26 @@ s3.put_public_access_block(
 print('Created safe bucket')
 
 #IAM below is vulnerable because it does not have MFA configured
-
 iam.create_user(UserName = 'no-mfa-user')
 print('Created IAM user with no MFA')
+
+#EC2 has security group with port 22 open to the entire world nooooooo!
+vpc = ec2.describe_vpcs() ['Vpcs'] [0] ['VpcId']
+sg = ec2.describe_security_group(
+    GroupName = 'vulnerable-sg',
+    Description = 'Intentionally misconfigured',
+    VpcId = vpc
+)['GroupId']
+
+ec2.authorize_security_group_ingress(
+    GroupId = sg,
+    IpPermission = [{
+        'IpProtocol' : 'tcp',
+        'FromPort' : 22,
+        'ToPort' : 22,
+        'IpRanges' : [{'CidrIp' : '0.0.0.0/0'}]
+    }]
+)
+
+print(f'Created vulnerable security group {sg}')
+print('\nLocalStack seeded. Ready for exercises.')
