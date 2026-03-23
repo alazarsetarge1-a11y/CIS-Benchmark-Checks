e420144b7a0e48308e9524dccc978205d15a5799
alazarsetarge1-a11y <alazarsetarge1@gmail.com>
2026-03-23 16:24:16 -0500

    Use generic Exception handlers & reformat
    Replace service-specific exception classes with a generic Exception in S3, IAM, and EC2 seeding blocks and normalize indentation for multi-line call arguments. This simplifies the control flow when resources already exist during localstack seeding, but note that broad Exception handlers can mask unexpected errors and may need to be scoped more narrowly in future.

--- seed_localstack.py
+++ seed_localstack.py
@@ -9,53 +9,53 @@ try:
     s3.create_bucket(Bucket = 'vulnerable-bucket')
     s3.delete_public_access_block(Bucket = 'vulnerable-bucket')
     print('Created vulnerable bucket')
-except s3.exceptions.BucketExists:
+except Exception:
     print('Bucket already exists, skipping')
 
 #S3 below is safe because public access is blocked
 try:
     s3.create_bucket(Bucket = 'safe-bucket')
     s3.put_public_access_block(
-    Bucket = 'safe-bucket',
-    PublicAccessBlockConfiguration = {
-        'BlockPublicAcls' : True,
-        'BlockPublicPolicy' : True,
-        'IgnorePublicAcls' : True,
-        'RestrictPublicBuckets' : True
-    }
-)
+        Bucket = 'safe-bucket',
+        PublicAccessBlockConfiguration = {
+            'BlockPublicAcls' : True,
+            'BlockPublicPolicy' : True,
+            'IgnorePublicAcls' : True,
+            'RestrictPublicBuckets' : True
+        }
+    )
     print('Created safe bucket')
 
-except s3.exceptions.VulBucketExists:
+except Exception:
     print('Bucket already exists, skipping')
 
 #IAM below is vulnerable because it does not have MFA configured
 try:
     iam.create_user(UserName='no-mfa-user')
     print('Created IAM user with no MFA')
-except iam.exceptions.EntityAlreadyExistsException:
+except Exception:
     print('IAM user already exists, skipping')
 
 #EC2 has security group with port 22 open to the entire world nooooooo!
 try:
     vpc = ec2.describe_vpcs() ['Vpcs'][0]['VpcId']
     sg = ec2.create_security_group(
-    GroupName = 'vulnerable-sg',
-    Description = 'Intentionally misconfigured',
-    VpcId = vpc
-)['GroupId']
+        GroupName = 'vulnerable-sg',
+        Description = 'Intentionally misconfigured',
+        VpcId = vpc
+    )['GroupId']
     ec2.authorize_security_group_ingress(
-    GroupId = sg,
-    IpPermissions = [{
-    'IpProtocol' : 'tcp',
-    'FromPort' : 22,
-    'ToPort' : 22,
-    'IpRanges' : [{'CidrIp' : '0.0.0.0/0'}]
-    }]
+        GroupId = sg,
+        IpPermissions = [{
+            'IpProtocol' : 'tcp',
+            'FromPort' : 22,
+            'ToPort' : 22,
+            'IpRanges' : [{'CidrIp' : '0.0.0.0/0'}]
+        }]
 )
     print(f'Created vulnerable security group {sg}')
     
-except ec2.exceptions.ClientError:
+except Exception:
     print('Security Group already exists, skipping')
 
 
