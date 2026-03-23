8e7b2b86dec8d28a5338cf91716e3d69c158b6af
alazarsetarge1-a11y <alazarsetarge1@gmail.com>
2026-03-23 16:06:08 -0500

    Update seed_localstack.py

--- seed_localstack.py
+++ seed_localstack.py
@@ -31,25 +31,28 @@ except iam.exceptions.EntityAlreadyExistsException:
     print('IAM user already exists, skipping')
 
 #EC2 has security group with port 22 open to the entire world nooooooo!
-vpc = ec2.describe_vpcs() ['Vpcs'][0]['VpcId']
-sg = ec2.create_security_group(
+try:
+    vpc = ec2.describe_vpcs() ['Vpcs'][0]['VpcId']
+    sg = ec2.create_security_group(
     GroupName = 'vulnerable-sg',
     Description = 'Intentionally misconfigured',
     VpcId = vpc
 )['GroupId']
-try:
-    ec2.authorize_security_group_ingress(
-    GroupId = sg,
-    IpPermissions = [{
-        'IpProtocol' : 'tcp',
-        'FromPort' : 22,
-        'ToPort' : 22,
-        'IpRanges' : [{'CidrIp' : '0.0.0.0/0'}]
-    }]
-)
     
 except ec2.exceptions.EntityExists:
     print('Security Group already exists, skipping')
+    
+ec2.authorize_security_group_ingress(
+GroupId = sg,
+IpPermissions = [{
+    'IpProtocol' : 'tcp',
+    'FromPort' : 22,
+    'ToPort' : 22,
+    'IpRanges' : [{'CidrIp' : '0.0.0.0/0'}]
+    }]
+)
+    
+
 
 print(f'Created vulnerable security group {sg}')
 print('\nLocalStack seeded. Ready for exercises.\n')
