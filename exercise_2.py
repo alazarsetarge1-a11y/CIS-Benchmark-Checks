dc62f879cd354b56329a5476ebc8743067ee7a98
alazarsetarge1-a11y <alazarsetarge1@gmail.com>
2026-03-27 17:49:10 -0500

    Append buckets with NO Block Config

--- exercise_2.py
+++ exercise_2.py
@@ -4,5 +4,25 @@ s3 = get_client('s3')
 
 response = s3.list_buckets()
 
+findings = []
+
 for bucket in response['Buckets']:
     name = bucket["Name"]
+
+try:
+    block = s3.get_public_access_block(Bucket=name)
+    config = block['PublicAccessBlockConfiguration']
+
+    if config['BlockPublicAcls'] == False:
+        findings.append({
+            'bucket' : name,
+            'severity' : 'Critical',
+            'issue' : 'Public access not blocked'
+        })
+
+except Exception: 
+    findings.append({
+        'bucket' : name,
+        'severity' : 'Critical',
+        'issue' : 'NO Block Config'
+    })
